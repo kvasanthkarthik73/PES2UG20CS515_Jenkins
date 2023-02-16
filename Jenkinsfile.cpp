fa8960677eb599b1a1b0d45021e7652e55f23fad
vasanth karthik <84130461+kvasanthkarthik73@users.noreply.github.com>
2023-02-17 00:09:42 +0530

    Update Jenkinsfile.cpp

--- Jenkinsfile.cpp
+++ Jenkinsfile.cpp
@@ -4,25 +4,25 @@ agent any
 stages {
 stage('Build') {
 steps {
-sh ‘mn clean install’
-echo ‘Build Stage Successful
+sh 'mn clean install'
+echo 'Build Stage Successful'
 }
 }
 stage('Test') {
 steps {
-sh ‘mvn test’
-echo ‘Test Stage Successful
+sh 'mvn test'
+echo 'Test Stage Successful'
 post {
 always {
-junit ‘target/surefire-reports/.xml’
+junit 'target/surefire-reports/.xml'
 }
 }
 }
 }
 stage( Deploy’) {
 steps {
-sh ‘mvn deploy’
-echo ‘Deployment Successful
+sh 'mvn deploy'
+echo 'Deployment Successful'
 }
 }
 }
