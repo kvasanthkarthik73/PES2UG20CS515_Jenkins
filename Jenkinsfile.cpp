febf28b4472d5eda6d95e5e9bb4e459c8054065a
vasanth karthik <84130461+kvasanthkarthik73@users.noreply.github.com>
2023-02-17 00:18:09 +0530

    Update Jenkinsfile.cpp

--- Jenkinsfile.cpp
+++ Jenkinsfile.cpp
@@ -1,30 +1,10 @@
 pipeline {
-
-agent any
-stages {
-stage('Build') {
-steps {
-sh 'PES2UG20CS515-1 clean install'
-echo 'Build Stage Successful'
-}
-}
-stage('Test') {
-steps {
-sh 'g++ hello.cpp -o hello'
-echo 'Test Stage Successful'
-
-}
-}
-stage( Deploy’) {
-steps {
-sh 'hello.cpp deploy'
-echo 'Deployment Successful'
-}
-}
-}
-post {
-failure {
-
-echo 'Pipeline failed’
-
+    agent { docker { image 'node:16.17.1-alpine' } }
+    stages {
+        stage('build') {
+            steps {
+                sh 'node --version'
+            }
+        }
+    }
 }
